509c46342f9beca2a9fab18bd74f050e9d370826
manovarun <samvarun@gmail.comn>
2019-09-19 11:04:40 +0530

    Lists & Passing State With Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component, Fragment } from 'react';
-import Navbar from './components/layout/Navbar';
-
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
 
 export class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar />
+        <div className="container">
+          <User />
+        </div>
       </Fragment>
     );
   }
