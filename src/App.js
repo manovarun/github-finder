07e82be87bc36c0ea808d436e59636b533ba2f55
manovarun <samvarun@gmail.comn>
2019-09-19 09:46:06 +0530

    Navbar Component, Props & PropTypes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+
+import './App.css';
 
 export class App extends Component {
   render() {
-    return <></>;
+    return (
+      <Fragment>
+        <Navbar />
+      </Fragment>
+    );
   }
 }
 
