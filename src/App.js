8790c2ec568ca0cea46b86e7c9f942ea834f994c
manovarun <samvarun@gmail.comn>
2019-09-23 23:22:42 +0530

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Axios from 'axios';
+import './App.css';
+
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
-
-import './App.css';
+import About from './components/pages/About';
 
 export class App extends Component {
   state = {
@@ -47,19 +49,32 @@ export class App extends Component {
   render() {
     const { loading, users } = this.state;
     return (
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </Fragment>
+      </Router>
     );
   }
 }
